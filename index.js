e64c695711d7b6538bf4885cfa5c65c12ff926b0
--global <--global>
2023-10-31 03:05:50 +0530

    let var const

---
+++ index.js
@@ -0,0 +1,84 @@
+// // var name='satish'
+// // console.log(name);
+
+// // name='kumar'
+// // console.log(name)
+ 
+// // name='chaubey';
+// // console.log(name)
+
+
+// var a=10
+// var b=10
+// var c=30
+
+// if(a==b){
+//     // console.log(a)
+//     // var b=1
+//     // console.log(b,"block")
+//     console.log(e,"function")
+// }
+
+// // console.log(e)
+// scopeTesting();
+
+// function scopeTesting(){
+//     // var b=2
+//     // console.log(b,"he")
+//     // a=20
+//     // console.log(a);
+//     // console.log(c)
+//     // var d='chaubey'
+//     // console.log(d)
+//     // console.log(e,"function")
+//     // var a
+//     console.log(e,"function")//undefined
+// }
+// // console.log(d)
+
+// const scopeTestingArrow= () =>{
+//     // var b=2
+//     // console.log(b,"Arrow")
+//     // a=20
+//     // console.log(a,"Arrow");
+//     // console.log(c,"Arrow")
+//     // var d='chaubey'
+//     // console.log(d)
+//     //  console.log(e,"function")
+//      e=100
+//     console.log(e,"function")
+// }
+
+// scopeTestingArrow()
+// var e
+// // console.log(d)
+
+// // let name='satish'
+// // console.log(name)
+
+
+
+//let 
+
+let a=10
+// a=30
+// console.log(a)
+// let a=10
+// console.log(a)
+console.log(a)
+
+if(a==a){
+    let b=20
+    console.log(a,"hello")
+    console.log(b)
+}
+console.log(b)
+
+function scopeTesting(){
+    let a=20
+    console.log(a)
+    console.log(b)
+
+}
+
+scopeTesting()
